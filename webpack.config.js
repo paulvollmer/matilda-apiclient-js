9efc64623dda5f7edbf3807005b5f6af8a13d848
Paul Vollmer <paul.vollmer@protonmail.com>
2019-07-05 21:59:42 +0200

    added missing babel/runtime dependency

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
